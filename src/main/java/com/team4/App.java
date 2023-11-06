e02e3803786c6fa3c9e3226eac0de2f9b26ec9f3
Moose <keshanmoosai@gmail.com>
2023-11-06 12:55:02 -0400

    New App main layout

--- src/main/java/com/team4/App.java
+++ src/main/java/com/team4/App.java
@@ -4,18 +4,9 @@ package com.team4;
  * Driver function of the program
  */
 public class App {
+
     public static void main(String[] args) {
-        
-        // Accept zip file containing submissions
-
-        // Extract contents of zip file into an internal collection of files
-    
-        // Run evaluation using provided test cases
-
-        // Produce PDF report based on evaluation results
-
-        System.out.println(sayHello());
+        JudgeSystem autoJudge = new JudgeSystem("Submissions.zip");
+        autoJudge.evaluateSubmissions();
     }
-
-    public static String sayHello() { return "Hello World"; }
 }
