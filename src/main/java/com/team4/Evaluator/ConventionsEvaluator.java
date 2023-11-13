dcba8ace684d9bd7c6b8e51119272f462de6c958
Keshan Moosai <keshanmoosai@gmail.com>
2023-11-13 17:00:42 -0400

    Added convention evalution cases

--- src/main/java/com/team4/Evaluator/ConventionsEvaluator.java
+++ src/main/java/com/team4/Evaluator/ConventionsEvaluator.java
@@ -5,9 +5,22 @@ import java.io.File;
 public class ConventionsEvaluator implements SyntaxEvaluator {
     
     private double score;
+    private String filename;
+    private String className;
+    private String constructorName;
+    private String methodName;
+    private boolean correctSignature;
 
     public ConventionsEvaluator() {
         this.score = 0.0;
+    }
+
+    private boolean testClassName(String className) {
+        return this.className.equals(className);
+    }
+
+    private boolean testConstructorName(String constructorName) {
+        return this.constructorName.equals(constructorName);
     }
 
     @Override
