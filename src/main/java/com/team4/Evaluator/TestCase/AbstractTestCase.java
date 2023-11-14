28a0edee7f4293a774c2c84cc21cddab5c229fa0
Keshan Moosai <keshanmoosai@gmail.com>
2023-11-14 18:48:03 -0400

    Updated ATC interface

--- src/main/java/com/team4/Evaluator/TestCase/AbstractTestCase.java
+++ src/main/java/com/team4/Evaluator/TestCase/AbstractTestCase.java
@@ -1,6 +1,7 @@
 package com.team4.Evaluator.TestCase;
 
 public interface AbstractTestCase {
-    public boolean testCriteria();
-    public boolean runTest();
+    public String getFeedbackComments();
+    public boolean testCriteria() throws Exception;
+    public boolean runTest() throws Exception;
 }
