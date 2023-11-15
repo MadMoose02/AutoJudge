1cce3cd9ebf3ebe522ed0e1dc55f4a49a3100c08
Dillon <dillonjookoo@gmail.com>
2023-11-15 00:32:53 -0400

    Updated super constructor parameter list.

--- src/main/java/com/team4/Evaluator/ConcreteTestCases/LuggageManifestTestCase.java
+++ src/main/java/com/team4/Evaluator/ConcreteTestCases/LuggageManifestTestCase.java
@@ -1,21 +1,67 @@
 package com.team4.Evaluator.ConcreteTestCases;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 import com.team4.Evaluator.TestCase.TestCase;
 
 public class LuggageManifestTestCase extends TestCase {
 
-    public LuggageManifestTestCase(String testName, File testFile) {
-        super(testName, testFile);
+    public LuggageManifestTestCase(String testName, File testFile, String[] parameters) {
+        super(testName, testFile, parameters);
     }
 
     public boolean testConstructor(){
+        try (Scanner scan = new Scanner(testFile)) {
+            while (scan.hasNext()) {
+
+                String line = scan.nextLine();
+                
+                if (line.contains("new") && line.contains("ArrayList<LuggageSlip>")){
+                    return true;
+                };
+                
+            }
+        }
+        catch (FileNotFoundException e) {
+            System.out.print("File Not Found");
+        }
 
         return false;
     }
 
     public boolean testAddLuggage(){
+        try (Scanner scan = new Scanner(testFile)) {
+            while (scan.hasNext()) {
+
+                String line = scan.nextLine();
+                
+                if (line.contains("new") && line.contains("LuggageSlip")){
+                    return true;
+                };
+                
+                if (line.contains("slips.add")){
+                    return true;
+                };
+                
+                if (line.contains("") && line.contains("LuggageSlip")){
+                    return true;
+                };
+                
+                if (line.contains("new") && line.contains("LuggageSlip")){
+                    return true;
+                };
+                
+                if (line.contains("new") && line.contains("LuggageSlip")){
+                    return true;
+                };
+                
+            }
+        }
+        catch (FileNotFoundException e) {
+            System.out.print("File Not Found");
+        }
 
         return false;
     }
@@ -41,6 +87,12 @@ public class LuggageManifestTestCase extends TestCase {
             return true;
         }
         return false;
+    }
+
+    @Override
+    public String getFeedbackComments() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getFeedbackComments'");
     }
     
 }
