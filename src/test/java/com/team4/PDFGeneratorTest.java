23eba81d1844a4a0b5ca4bf884a1948dbfa64d41
Keshan Moosai <keshanmoosai@gmail.com>
2023-11-15 01:30:34 -0400

    Updated PDF generation test code

--- src/test/java/com/team4/PDFGeneratorTest.java
+++ src/test/java/com/team4/PDFGeneratorTest.java
@@ -1,35 +1,36 @@
 package com.team4;
 
+import org.junit.Test;
+
+import java.io.File;
 import java.io.IOException;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.font.Standard14Fonts;
 
 
 public class PDFGeneratorTest {
-    public static void main(String[] args) {
-        try {
-            generatePDF();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void generatePDF() throws IOException {
+    
+    @Test
+    public void testGeneratePDF() throws IOException {
+        String testFilePath = System.getProperty("user.dir") + File.separator 
+            + "src" + File.separator + "test" + File.separator + "resources" 
+            + File.separator + "TestReport.pdf";
         PDDocument doc = new PDDocument();
         PDPage page = new PDPage();
         doc.addPage(page);
 
         PDPageContentStream contentStream = new PDPageContentStream(doc, page);
 
-        contentStream.setFont(PDType1Font.COURIER, 12);
+        contentStream.setFont(new PDType1Font(Standard14Fonts.FontName.TIMES_ROMAN), 12);
         contentStream.beginText();
-        contentStream.showText("Hello World");
+        contentStream.showText("Hello World\n\n\nThis is a sample report generation");
         contentStream.endText();
         contentStream.close();
 
-        doc.save("HelloWorld.pdf");
+        doc.save(testFilePath);
         doc.close();
     }
 }
