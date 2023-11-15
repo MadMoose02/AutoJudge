f8538563b18e1b951038d1f6b6cb417a42028926
Keshan Moosai <keshanmoosai@gmail.com>
2023-11-15 19:31:02 -0400

    Refactored code format

--- src/main/java/com/team4/Evaluator/HierarchyEvaluator.java
+++ src/main/java/com/team4/Evaluator/HierarchyEvaluator.java
@@ -1,9 +1,6 @@
 package com.team4.Evaluator;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
 import com.team4.Evaluator.ConcreteTestCases.AssociationHierarchyEvaluator;
 import com.team4.Evaluator.TestCase.*;
 
@@ -12,27 +9,15 @@ public class HierarchyEvaluator implements SyntaxEvaluator {
     private double score;
     private AbstractTestCollection testCollection;
 
+    /**
+     * Default constructor
+     */
     public HierarchyEvaluator() {
         this.score = 0.0;
         this.testCollection = new TestCollection();
     }
 
-    private double checksInheritance (File javaFile, String keyword){
-        try (Scanner scan = new Scanner(javaFile)) {
-            while (scan.hasNext()) {
-                String line = scan.nextLine();
-                
-                if (line.contains(keyword)){
-                    return 1.0;
-                }
-            } 
-        }
-
-        catch (FileNotFoundException e) {
-            System.out.print("File Not Found");
-        }
-        return 0.0;
-    }
+    // Methods
 
     @Override
     public double evaluate(File javaDocument) {
@@ -40,22 +25,28 @@ public class HierarchyEvaluator implements SyntaxEvaluator {
 
         if (filename.equals("LuggageManagementSystem")){
             this.testCollection.addTestCase(
-                new AssociationHierarchyEvaluator("Association check for '" + javaDocument.getName() + "'", 
-                                                    javaDocument, new String[]{"Flight"}
+                new AssociationHierarchyEvaluator(
+                    "Association check for '" + javaDocument.getName() + "'", 
+                    javaDocument, 
+                    new String[]{"Flight"}
                 )
             );
             
             this.testCollection.addTestCase(
-                new AssociationHierarchyEvaluator("Association check for '" + javaDocument.getName() + "'", 
-                                                    javaDocument, new String[]{"Passenger"}
+                new AssociationHierarchyEvaluator(
+                    "Association check for '" + javaDocument.getName() + "'", 
+                    javaDocument, 
+                    new String[]{"Passenger"}
                 )
             );
         }
 
         if (filename.equals("LuggageManifest")){
             this.testCollection.addTestCase(
-                new AssociationHierarchyEvaluator("Association check for '" + javaDocument.getName() + "'", 
-                                                    javaDocument, new String[]{"LuggageSlip"}
+                new AssociationHierarchyEvaluator(
+                    "Association check for '" + javaDocument.getName() + "'", 
+                    javaDocument, 
+                    new String[]{"LuggageSlip"}
                 )
             );
         }
@@ -63,13 +54,8 @@ public class HierarchyEvaluator implements SyntaxEvaluator {
         AbstractTestCollectionIterator iterator = this.testCollection.getIterator();
         while (iterator.hasNext()) {
             AbstractTestCase testCase = iterator.next();
-            try {
-
-                this.score = (testCase.runTest()) ? 1.0 : 0.0;
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            try { this.score = (testCase.runTest()) ? 1.0 : 0.0; } 
+            catch (Exception e) { System.out.println("\nTEST CASE FAILED"); }
         }
 
         return (double) this.score / (double) this.testCollection.size() * 100.0;
